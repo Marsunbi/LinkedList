51cd1a9dba27f6904c1c27557e1f6175ad1b8a7e
Martin <marsunbi@gmail.com>
2018-02-08 09:40:50 +0100

    Output formatting

--- Main.py
+++ Main.py
@@ -31,7 +31,7 @@ class LinkedList(object):
             tmp = tmp.next
             i += 1
             if tmp.next is None:
-                raise IndexError('Index out of bound at index {}'.format(index))
+                raise IndexError('Index out of bound. Index [{}]'.format(index))
         return tmp.value
 
     def remove(self, value):
@@ -71,6 +71,7 @@ class LinkedList(object):
             tmp.prev = node
             node.next = tmp
             self.first = node
+            return node.value
         else:
             i = 0
             node = self.Node(value)
@@ -82,6 +83,7 @@ class LinkedList(object):
             node.prev = tmp.prev
             tmp.prev = node
             node.next = tmp
+            return node.value
 
     def contains(self, value):
         tmp = self.first
@@ -114,24 +116,32 @@ class LinkedList(object):
 if __name__ == "__main__":
     ll = LinkedList()
     ll.add(2)
+    ll.add(5)
+    ll.add(1)
+    ll.add(10)
     ll.add(3)
     ll.add(5)
-    ll.add(10)
-    ll.add('test')
-    ll.add('hej')
-    print(ll)
-    print(ll.get(2))
-    print(ll.get(1))
+    ll.add(11)
+    print("This is the linked list: {}".format(ll))
+    print("")
     try:
-        print(ll.get(10))
+        print("Here I get the number on index 4: \t\t\t [{}]".format(ll.get(4)))
+        print("and on index 5: \t\t\t\t\t\t\t [{}]".format(ll.get(5)))
+        ll.get(15)
     except IndexError as e:
         print(e.args)
-    ll.remove('test')
-    print(ll)
-    ll.remove_at(2)
-    print(ll)
-    ll.insert(8, 2)
-    print(ll)
-    print(ll.contains(3))
-    print(ll.contains(95))
-    print(ll.size())
+    print("")
+    print("This is the current linked list: \t\t\t {}".format(ll))
+    print("The first node with value 5 is removed: \t {}".format(ll.remove(5)))
+    print("The list after the node has been deleted: \t {}".format(ll))
+    print("")
+    print("The node at index 3 is deleted: \t\t\t {}".format((ll.remove_at(3))))
+    print("The list after the node has been deleted: \t {} ".format(ll))
+    print("")
+    print("Node with value 8 inserted at index 2: \t\t {}".format(ll.insert(8, 2)))
+    print("The lidt after the node has been added: \t {}".format(ll))
+    print("")
+    print("Check if the list contains value 5: \t\t {}".format(ll.contains(5)))
+    print("Check if the list contains value 15: \t\t {}".format(ll.contains(15)))
+    print("")
+    print("The size of the linked list: \t\t\t\t {}".format(ll.size()))
